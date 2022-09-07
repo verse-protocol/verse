e9d6d9247800efd055478895048d9d02f147e532
james-c <j.a.cunningham@gmail.com>
2022-09-07 15:48:20 +0100

    Add holding index page

--- portal/src/routes/+page.svelte
+++ portal/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Verse Portal</h1>
+<p>A maze of twisty cryptographs, all different...</p>
