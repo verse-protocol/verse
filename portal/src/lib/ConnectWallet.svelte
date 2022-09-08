e87827849621f406959d2dcd3eb1718db75ac9c2
james-c <j.a.cunningham@gmail.com>
2022-09-08 14:43:32 +0100

    Show connected account on page load if already connected

--- portal/src/lib/ConnectWallet.svelte
+++ portal/src/lib/ConnectWallet.svelte
@@ -3,6 +3,7 @@
  export let web3Props = { provider: null, signer: null, account: null, chainId: null };
  // connect the wallet
  async function connectWallet() {
+     console.log("hello")
      let provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
      await provider.send('eth_requestAccounts', []);
      const signer = provider.getSigner();
@@ -10,6 +11,20 @@
      const chainId = await signer.getChainId();
      web3Props = { signer, provider, chainId, account };
  }
+
+ export async function checkWallet() {
+     console.log("checking wallet...")
+     let provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+     const signer = provider.getSigner();
+     const account = await signer.getAddress();
+     const chainId = await signer.getChainId();
+     web3Props = { signer, provider, chainId, account };
+ }
 </script>
 
-<button on:click={connectWallet}>Connect Wallet</button>
+{#if !web3Props.account}
+    <button on:click={connectWallet}>Connect Wallet</button>
+{:else}
+    Connected to {web3Props.account}
+{/if}
+
--- portal/src/routes/+page.svelte
+++ portal/src/routes/+page.svelte
@@ -1,19 +1,24 @@
 <script>
     import ConnectWallet from '$lib/ConnectWallet.svelte';
-
+    import { onMount } from 'svelte';
+    let connectWalletComponent;
+    
     export let web3Props = {
         provider: null,
         signer: null,
         account: null,
         chainId: null
     };
+    
+    onMount(function(){    
+    connectWalletComponent.checkWallet();
+    })
 </script>
+
 
 <h1>Verse Portal</h1>
 <p>A maze of twisty cryptographs, all different...</p>
 
-{#if !web3Props.account}
-    <ConnectWallet bind:web3Props />
-{:else}
-    Connected...
-{/if}
+<ConnectWallet bind:web3Props bind:this={connectWalletComponent} />
+
+
