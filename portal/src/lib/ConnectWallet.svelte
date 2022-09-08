0516f2feb827bdaeb618b77e509fd9a174cdeecb
james-c <j.a.cunningham@gmail.com>
2022-09-08 09:23:51 +0100

    Add basic metamask connection functionality

---
+++ portal/src/lib/ConnectWallet.svelte
@@ -0,0 +1,15 @@
+<script>
+ import { ethers } from 'ethers';
+ export let web3Props = { provider: null, signer: null, account: null, chainId: null };
+ // connect the wallet
+ async function connectWallet() {
+     let provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+     await provider.send('eth_requestAccounts', []);
+     const signer = provider.getSigner();
+     const account = await signer.getAddress();
+     const chainId = await signer.getChainId();
+     web3Props = { signer, provider, chainId, account };
+ }
+</script>
+
+<button on:click={connectWallet}>Connect Wallet</button>
--- portal/src/routes/+page.svelte
+++ portal/src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script>
+    import ConnectWallet from '$lib/ConnectWallet.svelte';
+
+    export let web3Props = {
+        provider: null,
+        signer: null,
+        account: null,
+        chainId: null
+    };
+</script>
+
 <h1>Verse Portal</h1>
 <p>A maze of twisty cryptographs, all different...</p>
+
+{#if !web3Props.account}
+    <ConnectWallet bind:web3Props />
+{:else}
+    Connected...
+{/if}
